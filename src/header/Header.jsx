201f782c564f63252ffbfdfc418547065fc2f9c6
Rahul <koderrahul@gmail.com>
2022-03-19 01:41:33 +0530

    2

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,8 +3,11 @@ import "./header.css"
 export default function Header() {
   return (
     <div className="header">
-        header
-
+        <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Blog</span>
+        </div>
+<img className="headerImg" src="./andre-francois-mckenzie-iGYiBhdNTpE-unsplash.jpg" alt="" />
     </div>
   )
 }
