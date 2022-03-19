b112da162dc7cf6bb22c601cc0d586bbf95beb0d
Rahul <koderrahul@gmail.com>
2022-03-19 11:59:16 +0530

    k

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -2,6 +2,12 @@ import"./sidebar.css"
 
 export default function Sidebar() {
   return (
-    <div className="sidebar">Sidebar</div>
+    <div className="sidebar">
+        <div className="sidebarItem">
+            <span className="sidebarTitle">About Me</span>
+   <img className = "Img" src="./zoltan-tasi-uNXmhzcQjxg-unsplash.jpg" alt="" /> 
+            <p> This is awesome</p>
+        </div>
+    </div>
   )
 }
