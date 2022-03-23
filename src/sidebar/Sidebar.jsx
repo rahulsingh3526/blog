fd3bf26c1e0feb2c19c6b699628d867223354894
Rahul <koderrahul@gmail.com>
2022-03-23 06:14:34 +0530

    23-03

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -6,8 +6,29 @@ export default function Sidebar() {
         <div className="sidebarItem">
             <span className="sidebarTitle">About Me</span>
    <img className = "Img" src="./zoltan-tasi-uNXmhzcQjxg-unsplash.jpg" alt="" /> 
-            <p> This is awesome</p>
+            <p> This is awesome and leets make it just like that i don't know what i am typying but i am trying to increase the lenght of this sentence</p>
         </div>
-    </div>
+
+        <div className="sidebarItem">
+        <span className="sidebarList">Categories</span>
+        <ul className="sidebarList">
+
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        
+        </ul>
+        
+        
+      </div>
+      <div className="sidebarItem">
+          <span className="sidebarTitle">Follow Us</span>
+          <div className="sidebarTitle"></div>
+          <div className="sidebarSocial"></div>
+      </div>
+      </div>
   )
 }
